f174a21a6e19b455547bd714c167606f7f5cb95b
Barney <holyja@naver.com>
2022-02-12 04:49:33 +0900

    Add: 음료 테이블까지 구현완료

--- products/models.py
+++ products/models.py
@@ -27,13 +27,13 @@ class Drink(models.Model):
 
 class Images(models.Model):
     image_url = models.CharField(max_length=2000)
-    drink = models.ForeignKey('Drink', on_delete=models.CASCADE)
+    drink     = models.ForeignKey('Drink', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'images'
 
 class Allergy(models.Model):
-    name = models.CharField(max_length=45)
+    name   = models.CharField(max_length=45)
     drinks = models.ManyToManyField('Drink', through='AllergyOfDrink', through_fields=('allergy', 'drink'))
 
     class Meta:
@@ -41,29 +41,29 @@ class Allergy(models.Model):
 
 class AllergyOfDrink(models.Model):
     allergy = models.ForeignKey('Allergy', on_delete=models.CASCADE)
-    drink = models.ForeignKey('Drink', on_delete=models.CASCADE)
+    drink   = models.ForeignKey('Drink', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'allergy_drink'
 
 class Size(models.Model):
-    name = models.CharField(max_length=45)
-    size_ml = models.CharField(max_length=45)
+    name             = models.CharField(max_length=45)
+    size_ml          = models.CharField(max_length=45)
     size_fluid_ounce = models.CharField(max_length=45)
-    drinks = models.ManyToManyField('Drink', through='Nutrition', through_fields=('size', 'drink'))
+    drinks           = models.ManyToManyField('Drink', through='Nutrition', through_fields=('size', 'drink'))
 
     class Meta:
         db_table = 'sizes'
 
 class Nutrition(models.Model):
     one_serving_kca = models.DecimalField(max_digits=10, decimal_places=2)
-    sodium_mg = models.DecimalField(max_digits=10, decimal_places=2)
+    sodium_mg       = models.DecimalField(max_digits=10, decimal_places=2)
     saturated_fat_g = models.DecimalField(max_digits=10, decimal_places=2)
-    sugars_g = models.DecimalField(max_digits=10, decimal_places=2)
-    protein_g = models.DecimalField(max_digits=10, decimal_places=2)
-    caffeine_mg = models.DecimalField(max_digits=10, decimal_places=2)
-    drink = models.ForeignKey('Drink', on_delete=models.CASCADE)
-    size = models.ForeignKey('Size', on_delete=models.CASCADE)
+    sugars_g        = models.DecimalField(max_digits=10, decimal_places=2)
+    protein_g       = models.DecimalField(max_digits=10, decimal_places=2)
+    caffeine_mg     = models.DecimalField(max_digits=10, decimal_places=2)
+    drink           = models.ForeignKey('Drink', on_delete=models.CASCADE)
+    size            = models.ForeignKey('Size', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'nutritions'
